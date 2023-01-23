54f3d54902be19e3056004071fa00e09d808ca40
raikyy <98322401+raikyy@users.noreply.github.com>
2023-01-23 16:38:31 +0500

    а

---
+++ script.js
@@ -0,0 +1,44 @@
+let cells = document.querySelectorAll('#field td');
+console.log(cells);
+
+let i = 0;
+function start(cells) {
+    for (let cell of cells) {
+        cell.addEventListener('click', function step(){
+            if (i % 2 == 0) {
+                this.textContent = 'X';
+            }
+            else {
+                this.textContent = 'O';
+            }
+            i++;
+            this.removeEventListener('click', step);
+            if (isWinner(cells) == true) {
+                alert(`Победитель ${this.textContent}`);
+            };
+        })
+    }
+}
+
+start(cells);
+
+function isWinner(cells) {
+    let combs = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent === cells[comb[1]].textContent && 
+            cells[comb[1]].textContent == cells[comb[2]].textContent &&
+            cells[comb[0]].textContent != '') {
+                return true
+            }
+    }
+    return false
+}
